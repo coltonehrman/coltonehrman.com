49fead2bfc5854d7a83135b6fd1405150501fe4d
Colton Ehrman <coltonje95@gmail.com>
2024-01-25 18:58:30 -0600

    fix: remove email out

--- src/email.js
+++ src/email.js
@@ -13,12 +13,11 @@ const transporter = nodemailer.createTransport({
 });
 
 export default (mail) => {
-  return Promise.all([
-    transporter.sendMail({
-      from: `"${mail.name}" <${mail.email}>`, // sender address
-      to: "coltonje95@gmail.com", // list of receivers
-      subject: mail.subject, // Subject line
-      text: `
+  return transporter.sendMail({
+    from: `"${mail.name}" <${mail.email}>`, // sender address
+    to: "coltonje95@gmail.com", // list of receivers
+    subject: mail.subject, // Subject line
+    text: `
             You have received an incoming mail from ${mail.name}.
             Name: ${mail.name}
             Phone: ${mail.phone}
@@ -27,17 +26,6 @@ export default (mail) => {
             Message:
             ${mail.message}
         `, // plain text body
-      // html: "<b>There is a new article. It's about sending emails, check it out!</b>", // html body
-    }),
-    transporter.sendMail({
-      from: '"Colton Ehrman" <coltonje95@gmail.com>', // sender address
-      to: mail.email, // list of receivers
-      subject: "Thank you for contacting me, will be in touch soon!", // Subject line
-      text: `
-            Thank you for contacting me on my website http://coltonehrman.com.
-            I have recieved your email and will be in contact with you ASAP!
-        `, // plain text body
-      // html: "<b>There is a new article. It's about sending emails, check it out!</b>", // html body
-    }),
-  ]);
+    // html: "<b>There is a new article. It's about sending emails, check it out!</b>", // html body
+  });
 };
