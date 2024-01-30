b81da639107ac3fd06c6c18c4683af041b2d9309
Colton Ehrman <coltonje95@gmail.com>
2024-01-29 23:20:41 -0600

    fix: remove blocking logic & fix email logic

--- server.js
+++ server.js
@@ -9,22 +9,12 @@ import email from "./src/email.js";
 
 const app = express();
 
-const blocklist = new Set(["176.111.174.153", "185.234.216.114"]);
-
 app.set("trust proxy", true);
 
-app.use((req, res, next) => {
+app.use((req, _res, next) => {
   console.log("* ip: ", req.ip);
   console.log("* url: ", req.originalUrl);
   console.log(req.protocol, req.hostname, req.baseUrl, req.headers);
-  next();
-});
-
-app.use(async (req, res, next) => {
-  if (blocklist.has(req.ip)) {
-    console.log("! Blocking IP: ", req.ip);
-    return res.end();
-  }
 
   next();
 });
@@ -32,15 +22,13 @@ app.use(async (req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/mail", async (req, res) => {
-  // const { name, phone, email, subject, message } = req.body;
-  console.log("getting mail");
+  console.log("getting email");
   console.log(req.body);
 
   try {
-    const [toMe, toThem] = await email(req.body);
+    const result = await email(req.body);
     console.log("Email was sent: ");
-    console.log("To me: ", toMe);
-    console.log("To them: ", toThem);
+    console.log("Result: ", result);
   } catch (e) {
     console.error("Something went wrong with sending email: ", e);
   }
